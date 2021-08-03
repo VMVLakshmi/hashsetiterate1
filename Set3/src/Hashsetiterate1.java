7b4b84db871c67dd0268a2d133ddb44d03e183db
hai <hai@LAPTOP-4DV9N6FJ>
2021-08-03 18:20:00 +0530

    10th commit
    Upload all files

---
+++ Set3/src/Hashsetiterate1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Hashsetiterate1 {
+
+	public static void main(String[] args) {
+		Set<String> hset = new HashSet<String>();
+		  hset.add("Chaitanya");
+	     hset.add("Rahul");
+	     hset.add("Tim");
+	     hset.add("Rick");
+	     hset.add("Harry");
+	 
+	     for (String temp : hset) {
+	        System.out.println(temp);
+	     }
+	  }
+}
